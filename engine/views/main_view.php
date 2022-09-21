20fc45730ce94519f69d145a618460f81ca43fc1
Sergey Kudelin <kudelin.work@bk.ru>
2022-09-21 15:13:53 +0300

    Fixing: dropdowns, modal(2)

--- engine/views/main_view.php
+++ engine/views/main_view.php
@@ -1,4 +1,31 @@
 <main class="main">
+    
+    <!-- registration success modal -->
+
+    <div class="modal-body">
+        <h2 class="fw-bold mb-0">Успешная регистрация!</h2>
+        <a href="?url=check_user">
+            <button type="button" class="btn btn-lg btn-primary mt-5 w-100 custom-btn submit-success-reg" data-bs-dismiss="modal">ОК</button>
+        </a>
+        <?php
+            /*
+            echo '<script language="javascript">';
+            echo <<<END
+                const regBtn = document.forms.reg-form.elements.registration;
+                const regSuccessModal = document.querySelector('.modal-body');
+                const submitReg = document.querySelector('.submit-success-reg');
+                regBtn.addEventListener('click', function showModal() {
+                    regSuccessModal.classList.toggle('open');
+                });
+                submitReg.addEventListener('click', hideModal() {
+                    regSuccessModal.classList.toggle('open');
+                });
+            END;
+            echo '</script>';
+            */
+        ?>
+    </div>
+
     <div class="overlay fixed">
 
         <!-- greating -->
--- public/js/script.js
+++ public/js/script.js
@@ -1,2 +1,12 @@
 document.querySelector('.header').setAttribute(`style`, `width:${window.innerWidth}px`);
 document.querySelector('.header').setAttribute(`style`, `height:${window.innerHeight}px`);
+
+const regBtn = document.forms.reg-form.elements.registration;
+const regSuccessModal = document.querySelector('.modal-body');
+const submitReg = document.querySelector('.submit-success-reg');
+regBtn.addEventListener('click', function showModal() {
+    regSuccessModal.classList.toggle('open');
+});
+submitReg.addEventListener('click', function hideModal() {
+    regSuccessModal.classList.toggle('open');
+});
